54c5e59933260977a413281c35dab4697489a176
Manuel Camilo <mcamilo@protonmail.com>
2019-09-30 11:46:28 -0400

    .

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,5 +4,5 @@ var totalScore = 0;
 // Computer picks a random number greater than or equal to 19 and less than 120
 var computerPick = Math.floor(Math.random() * (+120 - + 19)) + +19;
 
-
-// 4 onclick events for each function
+// display for total score
+document.getElementById("totalScoreDisplay").innerHTML = totalScore;
