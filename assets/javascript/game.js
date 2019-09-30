16f7bb923b89ede8133e7bfdda38ef6fb9997614
Manuel Camilo <mcamilo@protonmail.com>
2019-09-30 12:02:49 -0400

    .

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,12 @@
 // Variables
 var totalScore = 0;
+var numWins = 0;
+var numLosses = 0;
 
 // Computer picks a random number greater than or equal to 19 and less than 120
 var computerPick = Math.floor(Math.random() * (+120 - + 19)) + +19;
 
-// display for total score
+// display for total score, wins, and losses
 document.getElementById("totalScoreDisplay").innerHTML = totalScore;
+document.getElementById("lossesDisplay").innerHTML = "Losses: " + numLosses;
+document.getElementById("winsDisplay").innerHTML = "Wins: " + numWins;
