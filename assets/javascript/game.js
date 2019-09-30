8b6e592f342d83a665a20fb4ba68262a19f5d6d5
Manuel Camilo <mcamilo@protonmail.com>
2019-09-30 15:21:00 -0400

    .

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,6 @@
 var totalScore = 0;
 var numWins = 0;
 var numLosses = 0;
-var isValidScore = true; // make sure score is less than computers
 
 // Computer picks a random number greater than or equal to 19 and less than 120
 var computerPick = Math.floor(Math.random() * (+120 - + 19)) + +19;
@@ -13,45 +12,62 @@ var randomEmeraldValue = Math.floor(Math.random() * (+12 - + 1)) + +1;
 var randomSapphireValue = Math.floor(Math.random() * (+12 - + 1)) + +1;
 var randomCrystalValue = Math.floor(Math.random() * (+12 - + 1)) + +1;
 
-// display for total score, wins, and losses
+//
+function start() {
+    computerPick = Math.floor(Math.random() * (+120 - + 19)) + +19;
+
+    // Random number between 1 and 12 for each button
+    randomRubyValue = Math.floor(Math.random() * (+12 - + 1)) + +1;
+    randomEmeraldValue = Math.floor(Math.random() * (+12 - + 1)) + +1;
+    randomSapphireValue = Math.floor(Math.random() * (+12 - + 1)) + +1;
+    randomCrystalValue = Math.floor(Math.random() * (+12 - + 1)) + +1;
+}
+
+// display for total score, computer pick, wins, and losses
 document.getElementById("totalScoreDisplay").innerHTML = totalScore;
 document.getElementById("lossesDisplay").innerHTML = "Losses: " + numLosses;
 document.getElementById("winsDisplay").innerHTML = "Wins: " + numWins;
 document.getElementById("computerPickDisplay").innerHTML = "Your Target Score: " + computerPick;
 
 // on click event
-$(document).ready(function() {
-    $(".btn").on("click", function() {
-       // if (totalScore > computerPick) {
-         //   numWins++;
+$(document).ready(function () {
+    $(".btn").on("click", function () {
+        // if (totalScore > computerPick) {
+        //   numWins++;
         //}
 
         //if (isValidScore) { // don't run again
-          //  return false;
+        //  return false;
         //}
 
-        if ($(this).hasClass("ruby"))
+        if (totalScore > computerPick) {
+            numLosses++;
+            start();
+        }
+        else if (totalScore === computerPick)
         {
+            numWins++;
+            start();
+        }
+
+        if ($(this).hasClass("ruby")) {
             totalScore += randomRubyValue;
             document.getElementById("totalScoreDisplay").innerHTML = totalScore;
         }
-        else if ($(this).hasClass("emerald"))
-        {
+        else if ($(this).hasClass("emerald")) {
             totalScore += randomEmeraldValue;
             document.getElementById("totalScoreDisplay").innerHTML = totalScore;
 
         }
-        else if ($(this).hasClass("crystal"))
-        {
+        else if ($(this).hasClass("crystal")) {
             totalScore += randomCrystalValue;
             document.getElementById("totalScoreDisplay").innerHTML = totalScore;
 
         }
-        else if ($(this).hasClass("sapphire"))
-        {
+        else if ($(this).hasClass("sapphire")) {
             totalScore += randomSapphireValue;
             document.getElementById("totalScoreDisplay").innerHTML = totalScore;
-
         }
+
     });
 });
