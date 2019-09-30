23faff4684160fb274a8411fad1d558f05b20606
Manuel Camilo <mcamilo@protonmail.com>
2019-09-30 12:48:22 -0400

    .

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,10 +6,23 @@ var numLosses = 0;
 // Computer picks a random number greater than or equal to 19 and less than 120
 var computerPick = Math.floor(Math.random() * (+120 - + 19)) + +19;
 
+// Random number between 1 and 12 for each button
+var randomRubyValue = Math.floor(Math.random() * (+12 - + 1)) + +1;
+var randomEmeraldValue = Math.floor(Math.random() * (+12 - + 1)) + +1;
+var randomSapphireValue = Math.floor(Math.random() * (+12 - + 1)) + +1;
+var randomCrystalValue = Math.floor(Math.random() * (+12 - + 1)) + +1;
+
+
 // display for total score, wins, and losses
 document.getElementById("totalScoreDisplay").innerHTML = totalScore;
 document.getElementById("lossesDisplay").innerHTML = "Losses: " + numLosses;
 document.getElementById("winsDisplay").innerHTML = "Wins: " + numWins;
 document.getElementById("computerPickDisplay").innerHTML = "Target: " + computerPick;
 
-// 4 on click events for each button
+// on click event
+$(document).ready(function() {
+    $(".crystal").on("click", function() {
+        
+    });
+
+});
